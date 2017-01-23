55bd1c20050f752187d014c39b769c3b80c88994
Brian Holt <brian.holt@samsung.com>
2017-01-23 10:39:18 +0000

    Fixed formatting

--- moviepy_lane.py
+++ moviepy_lane.py
@@ -9,32 +9,32 @@ import sys
 
 
 def main(argv):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print('test.py -i <inputfile> -o <outputfile>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print('test.py -i <inputfile> -o <outputfile>')
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-   print('Input file is "', inputfile)
-   print('Output file is "', outputfile)
+    inputfile = ''
+    outputfile = ''
+    try:
+       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print('moviepy_lane.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('moveipy_lane.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    print('Input file is "', inputfile)
+    print('Output file is "', outputfile)
 
-   detector = lane.LaneDetector("calibration.p", debug=False)
+    detector = lane.LaneDetector("calibration.p", debug=False)
 
-   clip = VideoFileClip(inputfile)
-   project_clip = clip.fl_image(detector.process)
+    clip = VideoFileClip(inputfile)
+    project_clip = clip.fl_image(detector.process)
 
-   project_clip.write_videofile(outputfile, audio=False)
+    project_clip.write_videofile(outputfile, audio=False)
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
 
 
